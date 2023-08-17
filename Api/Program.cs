04834a9eda2d8c5114c7da343803756fd151e73e
Evangelos Boudis <evangelos.boudis@gmail.com>
2023-08-17 10:13:16 +0300

    Dependency Injection update (SQLite driver, Options configuration, IConfiguration direct reference to Infrastructure)

--- Api/Program.cs
+++ Api/Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddInfrastructure();
+builder.Services.AddInfrastructure(builder.Configuration);
 builder.Services.ConfigureOptions<ApiBehaviorOptionsSetup>();
 builder.Services.AddTransient<ExceptionHandlerMiddleware>();
 
--- Infrastructure/Injection/Extensions/ServiceCollectionExtensions.cs
+++ Infrastructure/Injection/Extensions/ServiceCollectionExtensions.cs
@@ -1,8 +1,10 @@
 using Application.Authentication;
 using Application.Keys;
 using Application.Notification;
+using Application.Notification.Data;
 using Application.Password;
 using Application.Token;
+using Application.Token.Data;
 using Infrastructure.Authentication;
 using Infrastructure.Injection.Options;
 using Infrastructure.Keys;
@@ -10,20 +12,23 @@ using Infrastructure.Notification;
 using Infrastructure.Password;
 using Infrastructure.Store;
 using Infrastructure.Token;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Infrastructure.Injection.Extensions;
 
 public static class ServiceCollectionExtensions
 {
-    public static void AddInfrastructure(this IServiceCollection services)
+    public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
     {
-        services.AddDbContext<DataContext>();
+        var connection = configuration.GetSection("Datasource").Get<string>()!;
+        services.AddDbContext<DataContext>(options => { options.UseSqlite(connection); });
 
         services.AddAuthentication().AddJwtBearer();
 
-        services.ConfigureOptions<TokenOptionsSetup>();
-        services.ConfigureOptions<MailOptionsSetup>();
+        services.Configure<MailOptions>(configuration.GetSection("Mail"));
+        services.Configure<TokenOptions>(configuration.GetSection("Token"));
 
         services.ConfigureOptions<TokenBearerOptionsSetup>();
         services.ConfigureOptions<AuthenticationOptionsSetup>();
@@ -33,5 +38,7 @@ public static class ServiceCollectionExtensions
         services.AddScoped<INotificationSender, NotificationSender>();
         services.AddScoped<ITokenProvider, TokenProvider>();
         services.AddScoped<IAuthService, AuthService>();
+
+        return services;
     }
 }
