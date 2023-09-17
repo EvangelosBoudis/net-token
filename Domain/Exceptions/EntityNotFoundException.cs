cfa1aac553fef1aaf1870daaf3fac9b9de77f2b5
Evangelos Boudis <evangelos.boudis@gmail.com>
2023-09-17 15:22:25 +0300

    no parameter constructor for moq object initialization

--- Domain/Exceptions/EntityNotFoundException.cs
+++ Domain/Exceptions/EntityNotFoundException.cs
@@ -2,8 +2,12 @@ namespace Domain.Exceptions;
 
 public class EntityNotFoundException<T> : Exception where T : class
 {
-    public EntityNotFoundException(object? identifier = null) : base(
+    public EntityNotFoundException(object identifier) : base(
         $"Entity: '{typeof(T).Name}' with ID: '{identifier}' not found.")
+    {
+    }
+
+    public EntityNotFoundException() : base($"Entity: '{typeof(T).Name}' not found.")
     {
     }
 }
